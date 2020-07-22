7ab568e204a6e32621a5b130e60c90b5f9d4ba22
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-22 17:17:12 -0300

    show image based on aspect ratio

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -30,7 +30,7 @@ function Feed() {
               <Name>{item.author.name}</Name>
             </Header>
 
-            <PostImage source={{uri: item.image}} />
+            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
 
             <Description>
               <Name>{item.author.name}</Name> {item.description}
