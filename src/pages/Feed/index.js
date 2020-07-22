b3006e0c5261908de7cf472613b08bbcb3595ba8
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-22 18:02:19 -0300

    add the use callbak hook

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -9,14 +9,35 @@ function Feed() {
   const [feed, setFeed] = useState([]);
   const [page, setPage] = useState(1);
 
-  async function loadPage(pageNumber = page, shoudRefresh) {
-    const {data} = await api.get(
-      `/feed?_expand=author&_limit=5&_page=${pageNumber}`,
-    );
+  const loadPage = useCallback(
+    async (pageNumber = page, shoudRefresh) => {
+      const {data} = await api.get(
+        `/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+      );
 
-    setFeed([...feed, ...data]);
-    setPage(pageNumber + 1);
-  }
+      setFeed([...feed, ...data]);
+      setPage(pageNumber + 1);
+    },
+    [page, setPage, setFeed, feed],
+  );
+
+  const renderItem = useCallback(
+    ({item}) => (
+      <Post>
+        <Header>
+          <Avatar source={{uri: item.author.avatar}} />
+          <Name>{item.author.name}</Name>
+        </Header>
+
+        <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
+
+        <Description>
+          <Name>{item.author.name}</Name> {item.description}
+        </Description>
+      </Post>
+    ),
+    [],
+  );
 
   useEffect(() => {
     loadPage();
@@ -29,20 +50,7 @@ function Feed() {
         keyExtractor={(post) => String(post.id)}
         onEndReached={() => loadPage()}
         onEndReachedThreshold={0.1}
-        renderItem={({item}) => (
-          <Post>
-            <Header>
-              <Avatar source={{uri: item.author.avatar}} />
-              <Name>{item.author.name}</Name>
-            </Header>
-
-            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
-
-            <Description>
-              <Name>{item.author.name}</Name> {item.description}
-            </Description>
-          </Post>
-        )}
+        renderItem={renderItem}
       />
     </View>
   );
