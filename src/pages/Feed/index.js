a4621505f69c07df0047a4baa88e5fb31947aee1
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-22 17:14:45 -0300

    add flatlist and styles consts

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList} from 'react-native';
 
-import {Container} from './styles';
+import api from '~/services/api';
+
+import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
 
 function Feed() {
-  return <Container />;
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {
+    async function loadFeed() {
+      const response = await api.get('/feed?_expand=author&_limit=5&_page=1');
+
+      setFeed(response.data);
+    }
+
+    loadFeed();
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={feed}
+        keyExtractor={(post) => String(post.id)}
+        renderItem={({item}) => (
+          <Post>
+            <Header>
+              <Avatar source={{uri: item.author.avatar}} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            <PostImage source={{uri: item.image}} />
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </View>
+  );
 }
 
 export default Feed;
--- src/pages/Main/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import {View} from 'react-native';
-
-const Main = () => <View />;
-
-export default Main;
