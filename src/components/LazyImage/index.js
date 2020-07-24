83ebcb7e2e1cebd381d9e27710923d5b98b7a47a
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-24 17:15:03 -0300

    add a timeout to see the image loading

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {Small, Original} from './styles';
 
 function LazyImage({smallSource, source, aspectRatio}) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  }, []);
+
+  function handleAnimate() {}
+
   return (
     <Small
       source={smallSource}
       ratio={aspectRatio}
       resizeMode="contain"
       blurRadius={2}>
-      <Original
-        source={source}
-        ratio={aspectRatio}
-        resizeMode="contain"
-        resizeMode="contain"
-      />
+      {loaded && (
+        <Original
+          source={source}
+          ratio={aspectRatio}
+          resizeMode="contain"
+          onLoadEnd={handleAnimate}
+        />
+      )}
     </Small>
   );
 }
